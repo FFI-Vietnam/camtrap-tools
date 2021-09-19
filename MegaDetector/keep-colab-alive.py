905a8ddbb3f1a77df8e4593720a3d581f4f6eade
panda1835 <phuc.le@ug.fuv.edu.vn>
2021-09-19 20:42:19 +0700

    add pseudo-interactive script to keep interacting with Colab notebook

---
+++ MegaDetector/keep-colab-alive.py
@@ -0,0 +1,17 @@
+"""
+Run this script to simulate double-click action after 30s. 
+It is used to keep Colab page interacted to avoid the 
+Colab notebook from stopping due to lack of interaction.
+
+Requirements: pynput, which can be installed by pip install pynput
+"""
+
+
+from pynput.mouse import Controller, Button
+import time
+mouse = Controller()
+while True:
+     mouse.click(Button.left, 2)
+     print('clicked')
+     time.sleep(30)
+ 
