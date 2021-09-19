9fcc7deca50d607088fc655c38c13683fdaac44c
panda1835 <phuc.le@ug.fuv.edu.vn>
2021-09-19 20:43:51 +0700

    text changes

--- MegaDetector/keep-colab-alive.py
+++ MegaDetector/keep-colab-alive.py
@@ -3,7 +3,7 @@ Run this script to simulate double-click action after 30s.
 It is used to keep Colab page interacted to avoid the 
 Colab notebook from stopping due to lack of interaction.
 
-Requirements: pynput, which can be installed by pip install pynput
+Requirements: pynput, which can be installed by `pip install pynput`
 """
 
 
